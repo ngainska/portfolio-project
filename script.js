9f8c52cbafac3031dab660f2fe76c65842b367b2
ngainska <87204580+ngainska@users.noreply.github.com>
2021-09-28 19:58:50 +0100

    added contact info draft

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
 let messageArray = ["Hello, my name is Nicole Gainska!"];
+//let secondArray = ["Welcome to the Contact Center!"];
 let textPosition = 0;
 let speed = 50;
 
 typewriter = () => {
-  document.querySelector("#type").innerHTML = messageArray[0].substring(0, textPosition)+ "<span  >\u25ae</span>" ;
+  document.querySelector("#type").innerHTML = messageArray[0].substring(0, textPosition)+ `<span class="main-title">\u25ae</span>` ;
+  //document.querySelector("#trial").innerHTML = secondArray[0].substring(0, textPosition)+ `<span class="main-title">\u25ae</span>` ;
 
   if(textPosition++ != messageArray[0].length)
   setTimeout(typewriter, speed,); 
   
+//(textPosition++ != secondArray[0].length)
 }
 
 window.addEventListener("load", typewriter);
