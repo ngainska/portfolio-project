846966ea6221a62db23fcdbab939b736e93ff582
ngainska <87204580+ngainska@users.noreply.github.com>
2021-10-01 13:20:50 +0100

    added links to home contact icons

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ let speed = 50;
 
 typewriter = () => {
   document.querySelector("#type").innerHTML = messageArray[0].substring(0, textPosition)+ `<span class="main-title">\u25ae</span>` ;
-  //document.querySelector("#trial").innerHTML = secondArray[0].substring(0, textPosition)+ `<span class="main-title">\u25ae</span>` ;
-
   if(textPosition++ != messageArray[0].length)
   setTimeout(typewriter, speed,); 
   
-//(textPosition++ != secondArray[0].length)
 }
+//typewriter2 = () => {
+  //document.querySelector("#trial").innerHTML = secondArray[0].substring(0, textPosition)+ `<span class="main-title">\u25ae</span>` ;
+  //if(textPosition++ != secondArray[0].length)
+  //setTimeout(typewriter, speed,); 
+  
+//}
 
 window.addEventListener("load", typewriter);
+
 
 //<span>⏷</span>
