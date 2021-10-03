c798def7af7370be105c233b41cb0646b5d9c9d5
ngainska <87204580+ngainska@users.noreply.github.com>
2021-10-03 15:00:55 +0100

    added seo and footers

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let messageArray = ["Hello, I'm Nicole Gainska!"];
-//let secondArray = ["Welcome to the Contact Center!"];
+let secondArray = ["Welcome to the Contact Center!"];
 let textPosition = 0;
 let speed = 40;
+
 
 typewriter = () => {
   document.querySelector("#type").innerHTML = messageArray[0].substring(0, textPosition)+ `<span class="main-title">\u25ae</span>` ;
@@ -9,13 +10,13 @@ typewriter = () => {
   setTimeout(typewriter, speed,); 
   
 }
-//typewriter2 = () => {
-  //document.querySelector("#trial").innerHTML = secondArray[0].substring(0, textPosition)+ `<span class="main-title">\u25ae</span>` ;
-  //if(textPosition++ != secondArray[0].length)
-  //setTimeout(typewriter, speed,); 
-  
-//}
-
 window.addEventListener("load", typewriter);
+
+
+  
+  
+
+
+
 
 //<span>⏷</span>
